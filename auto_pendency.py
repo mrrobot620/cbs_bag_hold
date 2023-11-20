6bde798c8e9479d5967387fd74483b767fe20415
abhishek.h1 <abhishek620380@gmail.com>
2023-11-21 04:05:56 +0530

    Try Method

--- auto_pendency.py
+++ auto_pendency.py
@@ -1,11 +1,12 @@
 import pandas as pd
 from datetime import datetime
 import pytz
-
+import logging
 
 time_zone = pytz.timezone('Asia/Kolkata')
 current_time= datetime.now(time_zone)
 
+logging.basicConfig(format='%(asctime)s %(message)s' , datefmt='%m/%d/%Y %I:%M:%S %p' , filename='auto_pendency.logs' , encoding='utf-8' , level=logging.DEBUG )
 
 # All the files needed 
 df_secondary = pd.read_csv('ykb_secondary_pending_abhi.csv')
@@ -36,86 +37,95 @@ try:
     print(f"Secondary Pending B5 PH: {sum(df_secondary_b5_ph1)}")
     print(f"Secondary Pending B5 SPH: {sum(df_secondary_b5_sph1)}")
     print(f"Secondary Total Pending B5: {sum(df_secondary_b5_ph1) + sum(df_secondary_b5_sph1)} ")
+    logging.debug(": Success Processing Secondary Pendency File")
 except Exception as E:
-    print("Failed while processing secondary file")
-    print(E)
+    logging.error(f"Error while Processing Secondary File:  {E}")
 
 
 # OutBond Pending Automation Part
-outbond_total = outbond_df['tracking_id_merchant'].sum()
-print(f"Total Outbond:  {outbond_total}")
-outbond_xd = pd.read_csv('ykb_outbond_crossdock_abhi.csv')
-outbond_xd_total = outbond_xd['tracking_id_merchant'].sum()
-print(f"Total Cross-Dock Outbond:  {outbond_xd_total}")
-outbond_sl = pd.read_csv('outbond_semi_large_abhi.csv')
-outbond_sl_total = outbond_sl['tracking_id_merchant'].sum()
-print(f"Total OutBond Semi-Large: {outbond_sl_total} ")
+try:
+    outbond_total = outbond_df['tracking_id_merchant'].sum()
+    print(f"Total Outbond:  {outbond_total}")
+    outbond_xd = pd.read_csv('ykb_outbond_crossdock_abhi.csv')
+    outbond_xd_total = outbond_xd['tracking_id_merchant'].sum()
+    print(f"Total Cross-Dock Outbond:  {outbond_xd_total}")
+    outbond_sl = pd.read_csv('outbond_semi_large_abhi.csv')
+    outbond_sl_total = outbond_sl['tracking_id_merchant'].sum()
+    print(f"Total OutBond Semi-Large: {outbond_sl_total} ")
+    logging.debug(": Success Processing OB Files")
+except Exception as E:
+    logging.error(f"Error while Processing OB Files:  {E}")
 
 
 
 ## PPPH Automation Part
-df_ppph_zo = df_ppph[df_ppph['bag_type_ph'] == "ZO"]
-df_ppph_zo_ph  = df_ppph_zo[df_ppph_zo['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani|Bhiwadi")]
-zo_ph_shipment_count  = df_ppph_zo_ph['tracking_id_ekart'].sum()
-df_ppph_zo_sph = df_ppph_zo[~df_ppph_zo.index.isin(df_ppph_zo_ph.index)]
-zo_sph_shipment_count = df_ppph_zo_sph['tracking_id_ekart'].sum()
-print(f"ZO PPPH PH: {zo_ph_shipment_count}")
-print(f"ZO PPPH SPH: {zo_sph_shipment_count}")
-print(f"Total ZO Pending:  {zo_ph_shipment_count + zo_sph_shipment_count}")
+try:
+    df_ppph_zo = df_ppph[df_ppph['bag_type_ph'] == "ZO"]
+    df_ppph_zo_ph  = df_ppph_zo[df_ppph_zo['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani|Bhiwadi")]
+    zo_ph_shipment_count  = df_ppph_zo_ph['tracking_id_ekart'].sum()
+    df_ppph_zo_sph = df_ppph_zo[~df_ppph_zo.index.isin(df_ppph_zo_ph.index)]
+    zo_sph_shipment_count = df_ppph_zo_sph['tracking_id_ekart'].sum()
+    print(f"ZO PPPH PH: {zo_ph_shipment_count}")
+    print(f"ZO PPPH SPH: {zo_sph_shipment_count}")
+    print(f"Total ZO Pending:  {zo_ph_shipment_count + zo_sph_shipment_count}")
+    df_ppph_b5 = df_ppph[df_ppph['bag_type_ph'] == "B5"]
+    df_ppph_b5_ph = df_ppph_b5[df_ppph_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani")]
+    b5_ph_shipment_count = df_ppph_b5_ph['tracking_id_ekart'].sum()
+    df_ppph_b5_sph = df_ppph_b5[~df_ppph_b5.index.isin(df_ppph_b5_ph.index)]
+    b5_sph_shipment_count = df_ppph_b5_sph['tracking_id_ekart'].sum()
+    print(f"B5 PPPH PH: {b5_ph_shipment_count}")
+    print(f"B5 PPPH SPH: {b5_sph_shipment_count}")
+    print(f"Total B5 Pending:  {b5_ph_shipment_count + b5_sph_shipment_count}")
+    logging.debug(f": Success PPPH File ")
+except Exception as E:
+    logging.debug(f": Error while Processing the PPPH Files : {E}")
 
-
-df_ppph_b5 = df_ppph[df_ppph['bag_type_ph'] == "B5"]
-df_ppph_b5_ph = df_ppph_b5[df_ppph_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani")]
-b5_ph_shipment_count = df_ppph_b5_ph['tracking_id_ekart'].sum()
-df_ppph_b5_sph = df_ppph_b5[~df_ppph_b5.index.isin(df_ppph_b5_ph.index)]
-b5_sph_shipment_count = df_ppph_b5_sph['tracking_id_ekart'].sum()
-
-
-print(f"B5 PPPH PH: {b5_ph_shipment_count}")
-print(f"B5 PPPH SPH: {b5_sph_shipment_count}")
-print(f"Total B5 Pending:  {b5_ph_shipment_count + b5_sph_shipment_count}")
 
 # Bagging Pending 
-df_bagging_zo = df_bagging[df_bagging['bag_type_ph'] == "ZO"]
-df_bagging_zo['bag_facility_source_name'].fillna("Not Found" , inplace=True)
-df_bagging_zo_ph = df_bagging_zo[df_bagging_zo['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwadi|Bhiwani")]
-df_bagging_zo_sph = df_bagging_zo[~df_bagging_zo.index.isin(df_bagging_zo_ph.index)]
-zo_ph_bagging_count = df_bagging_zo_ph.groupby("bag_facility_source_name").size()
-zo_sph_bagging_count = df_bagging_zo_sph.groupby('bag_facility_source_name').size()
-print(f"ZO PH Bagging Pending: {sum(zo_ph_bagging_count)}")
-print(f"ZO SPH Bagging Pending: {sum(zo_sph_bagging_count)} ")
-print(f"Total ZO Bagging Pending: {sum(zo_ph_bagging_count)  + sum(zo_sph_bagging_count)}")
-df_bagging_b5 = df_bagging[df_bagging['bag_type_ph']== "B5"]
-df_bagging_b5['bag_facility_source_name'].fillna("Not Found" , inplace=True)
-df_bagging_b5_ph = df_bagging_b5[df_bagging_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwadi|Bhiwani")]
-df_bagging_b5_sph = df_bagging_b5[~df_bagging_b5.index.isin(df_bagging_b5_ph.index)]
-b5_ph_bagging_count = df_bagging_b5_ph.groupby("bag_facility_source_name").size()
-b5_sph_bagging_count = df_bagging_b5_sph.groupby("bag_facility_source_name").size()
-print(f"B5 PH Bagging Pending:  {sum(b5_ph_bagging_count)}")
-print(f"B5 SPH Bagging Pending:  {sum(b5_sph_bagging_count)}")
-print(f"Total B5 Bagging Pending:   {sum(b5_ph_bagging_count)  + sum(b5_sph_bagging_count)}")
+try:
+    df_bagging_zo = df_bagging[df_bagging['bag_type_ph'] == "ZO"]
+    df_bagging_zo['bag_facility_source_name'].fillna("Not Found" , inplace=True)
+    df_bagging_zo_ph = df_bagging_zo[df_bagging_zo['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwadi|Bhiwani")]
+    df_bagging_zo_sph = df_bagging_zo[~df_bagging_zo.index.isin(df_bagging_zo_ph.index)]
+    zo_ph_bagging_count = df_bagging_zo_ph.groupby("bag_facility_source_name").size()
+    zo_sph_bagging_count = df_bagging_zo_sph.groupby('bag_facility_source_name').size()
+    print(f"ZO PH Bagging Pending: {sum(zo_ph_bagging_count)}")
+    print(f"ZO SPH Bagging Pending: {sum(zo_sph_bagging_count)} ")
+    print(f"Total ZO Bagging Pending: {sum(zo_ph_bagging_count)  + sum(zo_sph_bagging_count)}")
+    df_bagging_b5 = df_bagging[df_bagging['bag_type_ph']== "B5"]
+    df_bagging_b5['bag_facility_source_name'].fillna("Not Found" , inplace=True)
+    df_bagging_b5_ph = df_bagging_b5[df_bagging_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwadi|Bhiwani")]
+    df_bagging_b5_sph = df_bagging_b5[~df_bagging_b5.index.isin(df_bagging_b5_ph.index)]
+    b5_ph_bagging_count = df_bagging_b5_ph.groupby("bag_facility_source_name").size()
+    b5_sph_bagging_count = df_bagging_b5_sph.groupby("bag_facility_source_name").size()
+    print(f"B5 PH Bagging Pending:  {sum(b5_ph_bagging_count)}")
+    print(f"B5 SPH Bagging Pending:  {sum(b5_sph_bagging_count)}")
+    print(f"Total B5 Bagging Pending:   {sum(b5_ph_bagging_count)  + sum(b5_sph_bagging_count)}")
+    logging.warning(": Success while Processing Bagging File")
+except Exception as E:
+    logging.warning(f": Error while Processing the Bagging File : {E}")
 
-# PPPH Pending 
-df_zo = df[df['bag_type_ph'] == "ZO"]
-df_zo
-df_zo_ph = df_zo[df_zo["bag_facility_source_name"].str.contains("DEL_PL|Bilaspur|Bhiwani")]
-df_zo_ph_wise = df_zo_ph.groupby('bag_facility_source_name').size()
-print(sum(df_zo_ph_wise))
-df_zo_sph = df_zo[~df_zo.index.isin(df_zo_ph.index)]
-df_zo_sph_wise = df_zo_sph.groupby('bag_facility_source_name').size()
-print(sum(df_zo_sph_wise))
-
-df_b5 = df[df['bag_type_ph'] == "B5"]
-df_b5
-
-df_b5_ph = df_b5[df_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani")]
-df_b5_ph_wise  = df_b5_ph.groupby('bag_facility_source_name').size()
-print(sum(df_b5_ph_wise))
-
-
-df_b5_sph  = df_b5[~df_b5.index.isin(df_b5_ph.index)]
-df_b5_sph_wise = df_b5_sph.groupby('bag_facility_source_name').size()
-print(sum(df_b5_sph_wise))
+# PPPH Pending
+try: 
+    df_zo = df[df['bag_type_ph'] == "ZO"]
+    df_zo
+    df_zo_ph = df_zo[df_zo["bag_facility_source_name"].str.contains("DEL_PL|Bilaspur|Bhiwani")]
+    df_zo_ph_wise = df_zo_ph.groupby('bag_facility_source_name').size()
+    print(sum(df_zo_ph_wise))
+    df_zo_sph = df_zo[~df_zo.index.isin(df_zo_ph.index)]
+    df_zo_sph_wise = df_zo_sph.groupby('bag_facility_source_name').size()
+    print(sum(df_zo_sph_wise))
+    df_b5 = df[df['bag_type_ph'] == "B5"]
+    df_b5
+    df_b5_ph = df_b5[df_b5['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani")]
+    df_b5_ph_wise  = df_b5_ph.groupby('bag_facility_source_name').size()
+    print(sum(df_b5_ph_wise))
+    df_b5_sph  = df_b5[~df_b5.index.isin(df_b5_ph.index)]
+    df_b5_sph_wise = df_b5_sph.groupby('bag_facility_source_name').size()
+    print(sum(df_b5_sph_wise))
+    logging.warning(": Success while Processing the PPPH greater than 12 Aging File")
+except Exception as E:
+    logging.warning(f": Error while Processing the PPPH greater than 12 Aging File:  {E}")
 
 
 def calculate_and_categorize_time(dataframe, column_name, current_time  , bucket_name):
