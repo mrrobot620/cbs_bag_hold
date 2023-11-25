a109ffb1f4f68160c65fcf14b3781a87746b4fe5
Abhishek Rajput <abhishek620380@gmail.com>
2023-11-26 00:30:20 +0530

    Robust HAHAHAH

--- auto_pendency.py
+++ auto_pendency.py
@@ -4,6 +4,7 @@ import pytz
 import logging
 import pymysql
 
+
 time_zone = pytz.timezone('Asia/Kolkata')
 current_time= datetime.now(time_zone)
 
@@ -20,37 +21,37 @@ conn = pymysql.connect(
     cursorclass=pymysql.cursors.DictCursor
 )
 
-sql_tables = ["live_values" , "ppph_12_ph" , "ppph_12_sph" , "secondary_12_ph" , 'secondary_12_sph' , 'bagging_12_ph' , 'bagging_12_sph' , "outbound_total_12_live" , "outbound_total_xd_12_live" , "outbound_total_sl_12_live"]
+multi_value_sql_tables = ["live_values" , "ppph_12" , "ppph_12_zo" , "ppph_12_b5" , "secondary_12_zo" , 'secondary_12_b5' , 'bagging_12_zo' , 'bagging_12_b5' , "outbound_total_12_live" , "outbound_total_xd_12_live" , "outbound_total_sl_12_live" , 'other_mh_12' , "secondary_12" , 'bagging_12']
+              
+single_value_sql_table = ["live_ph" , 'live_sph' , "live_ppph" , "ppph_zo_ph" , "ppph_b5_ph" , "ppph_zo_sph" , "ppph_b5_sph" , "other_mh_ppph1" ,  "secondary_pending_zo_ph" , "secondary_pending_zo_sph", "secondary_pending_b5_ph" , "secondary_pending_b5_sph" , "secondary_pending_total_ph" , 'secondary_pending_total_sph' , "bagging_pending_zo_ph" , "bagging_pending_zo_sph" , "bagging_pending_b5_ph" , "bagging_pending_b5_sph" , "bagging_pending_total_ph" ,  "bagging_pending_total_sph" , "outbound_total_live" , "outbound_sl_live" ,  "outbound_xd_live" , "zo_ppph_total" , "b5_ppph_total" , 'other_mh_PH' , "other_mh_SPH" , "other_mh_Total"  ,"total_secondary_pending" , 'total_secondary_pending_zo' , "total_secondary_pending_b5" , "total_bagging_pending" , "total_bagging_pending_zo" , "total_bagging_pending_b5"]
 
-def table_creator():
+def single_value_table_creator():
     try:
         with conn.cursor() as cursor:
-            for table in sql_tables:
-                sql = f"CREATE TABLE IF NOT EXISTS {table} (bucket VARCHAR(255) PRIMARY KEY, count INT)"
+            for table in single_value_sql_table:
+                sql = f"CREATE TABLE IF NOT EXISTS {table} (datetime DATETIME , bucket VARCHAR(255) , count INT)"
                 cursor.execute(sql)
         conn.commit()
         print("Sql Table Created")
     except Exception as e:
-        logging.warning(f"Error in Creaating Table:  {e}")
+        logging.warning(f"Error in Creating Table:  {e}")
     # finally:
     #     conn.close()
 
-def table_truncator():
+def multi_value_table_creator():
     try:
         with conn.cursor() as cursor:
-            for table in sql_tables:
-                sql = f"TRUNCATE TABLE {sql}"
+            for table in multi_value_sql_tables:
+                sql = f"CREATE TABLE IF NOT EXISTS {table} (datetime DATETIME , bucket VARCHAR(255) , 12_24 INT , 24_48 INT , gt_48 INT)"
                 cursor.execute(sql)
-        conn.commit()
-        print("Table Truncated")
-    except Exception as e:
-        logging.warning(f"Error in Truncating the Table: {e}")
-    # finally:
-    #     conn.close()
+            conn.commit()
+        print("Multi Value Table Created")
+    except Exception as E:
+        logging.warning(f"Error in Creating Mutli Value Table: {E}")
+ 
+single_value_table_creator()
+multi_value_table_creator()
 
-        
-table_creator()
-table_truncator()
 
 def table_exists():
     query = f"SHOW TABLES"
@@ -101,6 +102,11 @@ try:
     # Spelling Mistake
 except Exception as E:
     logging.warning(f"Error while reading Outbound SL 12 File  {E}")
+try:
+    df_for_OMH = pd.read_csv("Pendency_automation_report_ageing_greater_than_12.csv")
+except Exception as E:
+    logging.warning(f"Error {E}")
+
 
 
 try:
@@ -145,6 +151,11 @@ try:
     b5_ph_shipment_count = df_ppph_b5_ph['tracking_id_ekart'].sum()
     df_ppph_b5_sph = df_ppph_b5[~df_ppph_b5.index.isin(df_ppph_b5_ph.index)]
     b5_sph_shipment_count = df_ppph_b5_sph['tracking_id_ekart'].sum()
+    other_mh_ph = other_mh[other_mh['bag_facility_source_name'].str.contains("DEL_PL|Bilaspur|Bhiwani|Bhiwadi")]
+    other_mh_ph_count = other_mh_ph['tracking_id_ekart'].sum()
+    other_mh_sph = other_mh[~other_mh.index.isin(other_mh_ph.index)]
+    other_mh_sph_count = other_mh_sph['tracking_id_ekart'].sum()
+
 except Exception as E:
     logging.debug(f": Error while Processing the PPPH Files : {E}")
 
@@ -165,6 +176,7 @@ except Exception as E:
     logging.warning(f": Error while Processing the Bagging File : {E}")
 
 try: 
+    df = df[df['bag_facility_current_name'] == "MotherHub_YKB"]
     df_zo = df[df['bag_type_ph'] == "ZO"]
     df_zo
     df_zo_ph = df_zo[df_zo["bag_facility_source_name"].str.contains("DEL_PL|Bilaspur|Bhiwani")]
@@ -179,6 +191,12 @@ try:
     df_b5_sph_wise = df_b5_sph.groupby('bag_facility_source_name').size()
 except Exception as E:
     logging.warning(f": Error while Processing the PPPH greater than 12 Aging File:  {E}")
+
+try:
+    df_mh_ykb = df_for_OMH[df_for_OMH['bag_facility_current_name'] == "MotherHub_YKB"]
+    df_other_mh = df_for_OMH[~df_for_OMH.index.isin(df_mh_ykb.index)]
+except Exception as E:
+    logging.warning(E)
 
 
 def calculate_and_categorize_time(dataframe, column_name, current_time  , bucket_name):
@@ -199,9 +217,14 @@ def calculate_and_categorize_time(dataframe, column_name, current_time  , bucket
     data = dataframe[bucket_name].value_counts()
     return data
 
-
-zo_ph_secondary_total = calculate_and_categorize_time(df_secondary_zo_ph , 'fact_updated_at' , current_time , "ZO Secondary Pending PH: ")
-zo_sph_secondary_total = calculate_and_categorize_time(df_secondary_zo_sph , 'fact_updated_at' , current_time , "ZO Secondary Pending SPH: ")
+try:
+    zo_ph_secondary_total = calculate_and_categorize_time(df_secondary_zo_ph , 'fact_updated_at' , current_time , "ZO Secondary Pending PH: ")
+except Exception as E:
+    logging.warning(E)
+try:
+    zo_sph_secondary_total = calculate_and_categorize_time(df_secondary_zo_sph , 'fact_updated_at' , current_time , "ZO Secondary Pending SPH: ")
+except Exception as E:
+    logging.warning(E)
 b5_ph_secondary_total = calculate_and_categorize_time(df_secondary_b5_ph , 'fact_updated_at' , current_time , "B5 Secondary Pending PH: ")
 b5_sph_secondary_total = calculate_and_categorize_time(df_secondary_b5_sph , 'fact_updated_at' , current_time , "B5 Secondary Pending SPH: ")
 outbond_12_pendency = calculate_and_categorize_time(outbond_12 , 'fact_updated_at' , current_time , "Outbond Pendency")
@@ -215,6 +238,8 @@ final_zo_ph = calculate_and_categorize_time(df_zo_ph , 'fact_updated_at' , curre
 final_zo_sph = calculate_and_categorize_time(df_zo_sph , 'fact_updated_at' , current_time , "ZO SPH")
 final_b5_ph = calculate_and_categorize_time(df_b5_ph , 'fact_updated_at' , current_time , "B5 PH")
 final_b5_sph = calculate_and_categorize_time(df_b5_sph , 'fact_updated_at' , current_time , "B5 SPH")
+other_mh_df = calculate_and_categorize_time(df_other_mh , 'fact_updated_at' , current_time , "Other MH Ageing")
+
 
 def listMaker(dataframe1 , dataframe2):
     data = {}
@@ -240,30 +265,45 @@ ppph_zo_ph = {"ZO PH":  zo_ph_shipment_count}
 ppph_b5_ph  = {"B5 PH":  b5_ph_shipment_count}
 ppph_zo_sph = {"ZO SPH": zo_sph_shipment_count}
 ppph_b5_sph = {"B5 SPH":  b5_sph_shipment_count}
+other_mh_PH = {"Other MH PH": other_mh_ph_count}
+other_mh_SPH = {"Other MH SPH": other_mh_sph_count}
+other_mh_Total = {"Other MH Total": other_mh_ph_count + other_mh_sph_count}
 secondary_pending_zo_ph = {"Secondary Pending ZO PH": sum(df_secondary_zo_ph1)}
 secondary_pending_b5_ph = {"Secondary Pending B5 PH": sum(df_secondary_b5_ph1)}
 secondary_pending_zo_sph = {"Secondary Pending ZO SPH":  sum(df_secondary_zo_sph1)}
 secondary_pending_b5_sph  = {"Secondary Pending B5 SPH":  sum(df_secondary_b5_sph1)}
 secondary_pending_total_ph = {"Secondary Pending Total PH": sum(df_secondary_zo_ph1) + sum(df_secondary_b5_ph1)}
 secondary_pending_total_sph = {"Secondary Pending Total SPH": sum(df_secondary_zo_sph1) + sum(df_secondary_b5_sph1)}
-ppph_12_ph  = listMaker(final_zo_ph , final_b5_ph)
-ppph_12_sph = listMaker(final_zo_sph , final_zo_sph)
-secondary_12_ph = listMaker(zo_ph_secondary_total , b5_ph_secondary_total)
-secondary_12_sph = listMaker(zo_sph_secondary_total ,b5_sph_secondary_total)
+total_secondary_pending = {"Secondary Pending Total": sum(df_secondary_zo_ph1) + sum(df_secondary_b5_ph1) + sum(df_secondary_zo_sph1) + sum(df_secondary_b5_sph1)}
+total_secondary_pending_zo = {"Secondary Pending ZO": sum(df_secondary_zo_ph1) + sum(df_secondary_zo_sph1)}
+total_secondary_pending_b5 = {"Secondary Pending B5": sum(df_secondary_b5_ph1) + sum(df_secondary_b5_sph1)}
+ppph_12_zo  = listMaker(final_zo_ph , final_zo_sph)
+ppph_12_b5 = listMaker(final_b5_ph , final_b5_sph)
+ppph_12 = listMaker(ppph_12_zo , ppph_12_b5)
+secondary_12_zo = listMaker(zo_ph_secondary_total , zo_sph_secondary_total)
+secondary_12_b5 = listMaker(b5_ph_secondary_total ,b5_sph_secondary_total)
+secondary_12 = listMaker(secondary_12_zo , secondary_12_b5)
 bagging_pending_zo_ph = {"Bagging Pending ZO PH":  sum(zo_ph_bagging_count)}
 bagging_pending_zo_sph = {"Bagging Pending ZO SPH": sum(zo_sph_bagging_count)}
 bagging_pending_b5_ph = {"Bagging Pending B5 PH": sum(b5_ph_bagging_count)}
 bagging_pending_b5_sph = {"Bagging Pending B5 SPH": sum(b5_sph_bagging_count)}
 bagging_pending_total_ph = {"Bagging Pending Total PH": sum(zo_ph_bagging_count) + sum(b5_ph_bagging_count)}
 bagging_pending_total_sph = {"Bagging Pending Total SPH":  sum(zo_sph_bagging_count) + sum(b5_sph_bagging_count)}
-bagging_12_ph = listMaker(zo_ph_bagging_total , b5_ph_bagging_total)
-bagging_12_sph = listMaker(zo_sph_bagging_total , b5_sph_bagging_total)
+total_bagging_pending = {"Total Bagging pending": sum(zo_ph_bagging_count)+ sum(zo_sph_bagging_count) + sum(b5_ph_bagging_count) + sum(b5_sph_bagging_count)}
+total_bagging_pending_zo = {"Total ZO Bagging Pending": sum(zo_ph_bagging_count) + sum(zo_sph_bagging_count)}
+total_bagging_pending_b5 = {"Total B5 Bagging Pending": sum(b5_ph_bagging_count) + sum(b5_sph_bagging_count)}
+bagging_12_zo = listMaker(zo_ph_bagging_total , zo_sph_bagging_total)
+bagging_12_b5 = listMaker(b5_ph_bagging_total , b5_sph_bagging_total)
+bagging_12 = listMaker(bagging_12_zo , bagging_12_b5)
 outbound_total_live = {"Outbound Total Live":  outbound_total}
 outbound_sl_live = {"Outbound SL Live": outbound_sl_total}
 outbound_xd_live = {"Outbound XD Live": outbound_xd_total}
 outbound_total_12_live = listMaker1(outbond_12_pendency)
 outbound_total_xd_12_live = listMaker1(outbound_12_xd_total)
 outbound_total_sl_12_live = listMaker1(outbound_12_sl_total)
+zo_ppph_total = {"ZO PPPH Total": zo_ph_shipment_count + zo_sph_shipment_count}
+b5_ppph_total = {"B5 PPPH Total": b5_ph_shipment_count + b5_sph_shipment_count}
+other_mh_12 = listMaker1(other_mh_df)
 
 print(f"Live PH: {live_ph}")
 print(f"Live SPH: {live_sph}")
@@ -290,15 +330,17 @@ print(outbound_sl_live)
 print(outbound_xd_live)
 
 
-print(f"PPPH_PH_12: {ppph_12_ph}")
-print(f"PPPH SPH A12: {ppph_12_sph}")
-print(f"Secondary Pending PH 12:  {secondary_12_ph}")
-print(f"Secondary Pending SPH 12:  {secondary_12_sph}")
-print(f"Bagging Pending PH 12: {bagging_12_ph}")
-print(f"Bagging Pending SPH 12: {bagging_12_sph}")
+print(f"PPPH_PH_12: {ppph_12_zo}")
+print(f"PPPH SPH A12: {ppph_12_b5}")
+print(f"Secondary Pending PH 12:  {secondary_12_zo}")
+print(f"Secondary Pending SPH 12:  {secondary_12_b5}")
+print(f"Bagging Pending PH 12: {bagging_12_zo}")
+print(f"Bagging Pending SPH 12: {bagging_12_b5}")
 print(f"Outbound 12: {outbound_total_12_live}")
 print(f"Outbound 12 XD: {outbound_total_xd_12_live}")
 print(f"Outbound 12 SL:  {outbound_total_sl_12_live}")
+
+
 
 def ageing_to_sql(item, table_name):
     try:
@@ -315,57 +357,86 @@ def ageing_to_sql(item, table_name):
         print(f"Error: {e}")
         conn.rollback()  
 
-def dict_to_sql(item):
+def dict_to_sql(item, table_name):
     try:
         with conn.cursor() as cursor:
             for key, value in item.items():
                 insert_query = """
-                    INSERT INTO live_values (bucket, count)
-                    VALUES (%s, %s)
-                    ON DUPLICATE KEY UPDATE count = %s
-                """
-                # Pass the value twice for insert and update
-                cursor.execute(insert_query, (key, value, value))
+                    INSERT INTO {table_name} (datetime, bucket, count)
+                    VALUES (%s , %s, %s)
+                """.format(table_name=table_name)
+                cursor.execute(insert_query, (current_time , key , value))
             conn.commit()
     except Exception as e:
         print(f"Error: {e}")
-        conn.rollback()  # Rollback changes in case of an error
-    finally:
-        cursor.close()
+        conn.rollback()  
 
+def ageing_to_sql(item, table_name):
+    try:
+        with conn.cursor() as cursor:
+            keys = ['12-24 hours', '24-48 hours', '> 48 hours']
+            values = {'hours_12_24': 0, 'hours_24_48': 0, 'hours_gt_48': 0}
+            for key in keys:
+                if key == '12-24 hours':
+                    values['12_24'] = item.get(key, 0)
+                elif key == '24-48 hours':
+                    values['24_48'] = item.get(key, 0)
+                elif key == '> 48 hours':
+                    values['gt_48'] = item.get(key, 0)
+            insert_query = f"INSERT INTO {table_name} (datetime, bucket, 12_24, 24_48, gt_48) VALUES (%s, %s, %s, %s, %s)"
+            cursor.execute(insert_query, (current_time, {table_name}, values['12_24'], values['24_48'], values['gt_48']))
+            conn.commit()
+    except Exception as e:
+        print(f"Error: {e}")
+        conn.rollback()
 
-
-dict_to_sql(live_ph)
-dict_to_sql(live_sph)
-dict_to_sql(live_ppph)
-dict_to_sql(ppph_zo_ph)
-dict_to_sql(ppph_b5_ph)
-dict_to_sql(ppph_zo_sph)
-dict_to_sql(ppph_b5_sph)
-dict_to_sql(other_mh_ppph1)
-dict_to_sql(secondary_pending_zo_ph)
-dict_to_sql(secondary_pending_b5_ph)
-dict_to_sql(secondary_pending_zo_sph)
-dict_to_sql(secondary_pending_b5_sph)
-dict_to_sql(secondary_pending_total_ph)
-dict_to_sql(secondary_pending_total_sph)
-dict_to_sql(bagging_pending_zo_ph)
-dict_to_sql(bagging_pending_zo_sph)
-dict_to_sql(bagging_pending_b5_ph)
-dict_to_sql(bagging_pending_b5_sph)
-dict_to_sql(bagging_pending_total_ph)
-dict_to_sql(bagging_pending_total_sph)
-dict_to_sql(outbound_total_live)
-dict_to_sql(outbound_sl_live)
-dict_to_sql(outbound_xd_live)
-ageing_to_sql(ppph_12_ph , "ppph_12_ph")
-ageing_to_sql(ppph_12_sph , "ppph_12_sph")
-ageing_to_sql(secondary_12_ph , "secondary_12_ph")
-ageing_to_sql(secondary_12_sph , 'secondary_12_sph')
-ageing_to_sql(bagging_12_ph , 'bagging_12_ph')
-ageing_to_sql(bagging_12_sph , 'bagging_12_sph')
+dict_to_sql(zo_ppph_total , "zo_ppph_total")
+dict_to_sql(b5_ppph_total , "b5_ppph_total")
+dict_to_sql(other_mh_PH , 'other_mh_PH')
+dict_to_sql(other_mh_SPH , 'other_mh_SPH')
+dict_to_sql(other_mh_Total , 'other_mh_Total')
+dict_to_sql(live_ph , 'live_ph')
+dict_to_sql(live_sph , 'live_sph')
+dict_to_sql(live_ppph , "live_ppph")
+dict_to_sql(ppph_zo_ph , "ppph_zo_ph")
+dict_to_sql(ppph_b5_ph , "ppph_b5_ph")
+dict_to_sql(ppph_zo_sph , "ppph_zo_sph")
+dict_to_sql(ppph_b5_sph , "ppph_b5_sph") 
+dict_to_sql(other_mh_ppph1 , "other_mh_ppph1")
+dict_to_sql(secondary_pending_zo_ph , "secondary_pending_zo_ph")
+dict_to_sql(secondary_pending_b5_ph , 'secondary_pending_b5_ph')
+dict_to_sql(secondary_pending_zo_sph , "secondary_pending_zo_sph")
+dict_to_sql(secondary_pending_b5_sph , 'secondary_pending_b5_sph')
+dict_to_sql(secondary_pending_total_ph , "secondary_pending_total_ph")
+dict_to_sql(secondary_pending_total_sph , "secondary_pending_total_sph")
+dict_to_sql(total_secondary_pending , 'total_secondary_pending')
+dict_to_sql(total_secondary_pending_zo, "total_secondary_pending_zo")
+dict_to_sql(total_secondary_pending_b5 , "total_secondary_pending_b5")
+dict_to_sql(total_bagging_pending , "total_bagging_pending")
+dict_to_sql(total_bagging_pending_zo , "total_bagging_pending_zo")
+dict_to_sql(total_bagging_pending_b5 , "total_bagging_pending_b5")
+dict_to_sql(bagging_pending_zo_ph , 'bagging_pending_zo_ph')
+dict_to_sql(bagging_pending_zo_sph , "bagging_pending_zo_sph")
+dict_to_sql(bagging_pending_b5_ph , "bagging_pending_b5_ph")
+dict_to_sql(bagging_pending_b5_sph , "bagging_pending_b5_sph")
+dict_to_sql(bagging_pending_total_ph , "bagging_pending_total_ph") 
+dict_to_sql(bagging_pending_total_sph , 'bagging_pending_total_sph')
+dict_to_sql(outbound_total_live , 'outbound_total_live')
+dict_to_sql(outbound_sl_live , 'outbound_sl_live')
+dict_to_sql(outbound_xd_live , "outbound_xd_live")
+ageing_to_sql(ppph_12 , "ppph_12")
+ageing_to_sql(ppph_12_zo , "ppph_12_zo")
+ageing_to_sql(ppph_12_b5 , "ppph_12_b5")
+ageing_to_sql(secondary_12_zo , "secondary_12_zo")
+ageing_to_sql(secondary_12_b5 , 'secondary_12_b5')
+ageing_to_sql(bagging_12_zo , 'bagging_12_zo')
+ageing_to_sql(bagging_12_b5 , 'bagging_12_b5')
 ageing_to_sql(outbound_total_12_live , 'outbound_total_12_live')
 ageing_to_sql(outbound_total_xd_12_live , 'outbound_total_xd_12_live')
 ageing_to_sql(outbound_total_sl_12_live  , 'outbound_total_sl_12_live')
+ageing_to_sql(other_mh_12 , 'other_mh_12')
+ageing_to_sql(secondary_12 , 'secondary_12')
+ageing_to_sql(bagging_12 , 'bagging_12')
 print(table_exists())
+print(secondary_12)
 conn.close()
